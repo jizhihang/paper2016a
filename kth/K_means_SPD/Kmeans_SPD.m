159131591c01930f48aaba823ad607e1b584176f
johanna-codes <johacarvajalg@gmail.com>
2016-02-12 15:30:08 +1000

    Starting Kmeans over spd

--- kth/K_means_SPD/Kmeans_SPD.m
+++ kth/K_means_SPD/Kmeans_SPD.m
@@ -36,6 +36,6 @@ save_initial_clusters(path, load_sub_path, K, cluster_idx_pac);
 %for i=i:n_iter
     
   [cluster_list n_points_cl] = assign_points(list_pac, K,path, load_sub_path, total_num_covs);
-  
+  get_centers (cluster_list, n_points_cl, path, load_sub_path, K, dim)
     
 %end
