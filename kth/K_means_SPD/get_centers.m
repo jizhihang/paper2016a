b2d47b07efa6d3bb47469db3b79c6080eaa39c02
johanna-codes <johacarvajalg@gmail.com>
2016-02-12 11:10:25 +1000

    Starting Kmeans over spd

---
+++ kth/K_means_SPD/get_centers.m
@@ -0,0 +1,18 @@
+function S_cluster = get_centers (path_covs,list_Cov_points, num_points)
+
+
+
+for j =1:num_points
+    
+    
+   %load_Cov =  strcat(path, '/Cov_', all_people (pe) << "_" << actions(act) << "_segm" << num_covs <<  ".h5";
+
+    
+end
+
+
+
+
+
+
+
---
+++ kth/K_means_SPD/get_list.m
@@ -0,0 +1,22 @@
+function list_pac = get_list(n_people, n_actions, path)
+
+list_pac = cell(n_people*n_actions,3);
+k =1;
+load_sub_path =strcat('./overlapped_covariances/Covs/sc1/scale', int2str(scale_factor), '-shift',  int2str(shift));
+
+
+
+for pe = 1: n_people
+    for act=1: n_actions
+      list_pac{k,1}  = all_people(pe);      
+      list_pac{k,2}  = actions(act);
+      load_Numcov =  strcat( path, load_sub_path, '/NumCov_', all_people(pe), '_', actions(act),  '.dat');
+      num_cov = load( char(load_Numcov) );
+      list_pac{k,3}  = num_cov;
+      %list_pac{k,:}
+      k=k+1;
+      %pause
+        
+    end
+    
+end
