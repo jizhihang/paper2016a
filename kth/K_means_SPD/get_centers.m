8282b5e153e3cf80ba31e39396c6a5484f35fdb5
johanna-codes <johacarvajalg@gmail.com>
2016-02-12 15:29:10 +1000

    Starting Kmeans over spd

--- kth/K_means_SPD/get_centers.m
+++ kth/K_means_SPD/get_centers.m
@@ -1,27 +1,42 @@
-function get_centers (cluster_list, n_points_cl, path, load_sub_path, K)
+function get_centers (cluster_list, n_points_cl, path, load_sub_path, K, dim)
 
 
 
-for k =1:K
+for k = 1:K
     
-num_points_k = n_points_cl(k);
+    load_cluster =  strcat('./clusters_spd/cluster_', num2str(k), '_out_', num2str(K), '.h5' );
+    Sc = char(load_cluster);
+    data_one_cluster= hdf5info(Sc);
+    old_cluster_k = hdf5read(data_one_cluster.GroupHierarchy.Datasets(1));
+    
+    new_cluster_k = zeros(dim,dim);
+    num_points_k = n_points_cl(k);
+    
+    if (num_points_k ==0)
+        disp('You have a serious problem')
+        pause
+    end
+   
+   
+    
+    for p=1:num_points_k
+        
+        person =  cluster_list{ p, k}{1};
+        action =  cluster_list{ p, k}{2};
+        idx_cov = cluster_list{ p, k}{3};
+        
+        load_cov =  strcat( path, load_sub_path, '/Cov_', person, '_', action,  '_segm', num2str(idx_cov) , '.h5' );
+        S = char(load_cov);
+        data_one_cov= hdf5info(S);
+        cov_p = hdf5read(data_one_cov.GroupHierarchy.Datasets(1));
+        new_cluster_k = new_cluster_k + inv(0.5*(cov_p + old_cluster_k ));
 
-if (num_points_k ==0)
-    disp('You have a serious problem')
-    pause
-for p=1:num_points_k
+    end
     
-    person =  cluster_list{ p, k}{1};
-    action =  cluster_list{ p, k}{2};
-    idx_cov = cluster_list{ p, k}{3};
+    new_cluster_k = inv(new_cluster_k/num_points_k);
     
-    load_cov =  strcat( path, load_sub_path, '/Cov_', person, '_', action,  '_segm', num2str(idx_cov) , '.h5' );
-    S = char(load_cov);
-    data_one_cov= hdf5info(S);
-    cov = hdf5read(data_one_cov.GroupHierarchy.Datasets(1));
-    
-    
-end
+    save_cluster =  strcat('./clusters_spd/cluster_', num2str(k), '_out_', num2str(K), '.h5' );
+    hdf5write(save_cluster, '/dataset1', new_cluster_k);
     
 end
 
