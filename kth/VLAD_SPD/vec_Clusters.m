f2bfc3e4e3c3a41ae15835a89b65faf43c709bf9
johanna-codes <johacarvajalg@gmail.com>
2016-02-15 15:43:06 +1000

    Adding VLAD

--- kth/VLAD_SPD/vec_Clusters.m
+++ kth/VLAD_SPD/vec_Clusters.m
@@ -5,7 +5,7 @@ dim_spdvec  = dim*( dim + 1 )/2;
 
 for k=1: K
     
-     load_cluster =  strcat(path, './', folder_name_cl, '/cluster_', num2str(k), '_out_', num2str(K), '.h5' );
+     load_cluster =  strcat(path, 'K_means_SPD/', folder_name_cl, '/cluster_', num2str(k), '_out_', num2str(K), '.h5' );
      S = char(load_cluster);
      data_one_cluster= hdf5info(S);
      cluster_k = hdf5read(data_one_cluster.GroupHierarchy.Datasets(1));
