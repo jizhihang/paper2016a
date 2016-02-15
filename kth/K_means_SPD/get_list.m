e1cac9c96384f296a46ee400e0973187f1d8f15b
johanna-codes <johacarvajalg@gmail.com>
2016-02-15 11:09:54 +1000

    Run with K=256

--- kth/K_means_SPD/get_list.m
+++ kth/K_means_SPD/get_list.m
@@ -17,13 +17,12 @@ for pe = 1: n_people
     for act=1: n_actions
         
       list_pac{k,1}  = all_people(idx);
-      pause
       list_pac{k,2}  = actions(act);
       load_Numcov =  strcat( path, load_sub_path, '/NumCov_', all_people(idx), '_', actions(act),  '.dat');
       num_cov = load( char(load_Numcov) );
       total_num_covs = total_num_covs + num_cov;
       list_pac{k,3}  = num_cov;
-      list_pac{k,:}
+      [list_pac{k,:}]
       k=k+1;
       
         
