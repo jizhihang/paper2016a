cd128919945e3b279f15350cde03aac951d3268a
johanna-codes <johacarvajalg@gmail.com>
2016-02-16 14:34:56 +1000

    Adding VLAD

--- kth/VLAD_SPD/VLAD_main.m
+++ kth/VLAD_SPD/VLAD_main.m
@@ -20,21 +20,51 @@ shift = 0;
 %%
 path  = '~/codes/codes-git/paper2016a/trunk/kth/';
 load_sub_path =strcat('overlapped_covariances/Covs/sc1/scale', int2str(scale_factor), '-shift',  int2str(shift));
-
-people_test =   [ 2 3 5 6 7  8  9  10 22 ];
 n_actions = length(actions);
+%vec_Clusters(path, folder_name_cl, K, dim);
+
+%% Getting descriptors for Testing Set
+% people_test =   [ 2 3 5 6 7  8  9  10 22 ];
+% 
+% 
+% %pac : people, action, cells
+% [list_pac_te total_num_covs_te] = get_list( n_actions, path, all_people, actions, load_sub_path, people_test);
+% 
+% vec_TestingSet (path, load_sub_path, list_pac_te, dim );
+% 
+% 
+% 
+% 
+% for i=1:length(list_pac_te)
+%     i
+%     one_video_pac = {list_pac_te{i,:}};  
+%     tic
+%     [cluster_list_one_video n_points_cl] = assign_points(one_video_pac, K,path, load_sub_path, folder_name_cl);
+%     toc
+%     
+%     disp('Get VLAD descriptors')
+%     tic
+%     get_vlad_descriptors (one_video_pac, cluster_list_one_video, n_points_cl, dim, K);
+%     toc
+% 
+% end
+
+%%
+
+%% Getting descriptors for Testing Set
+people_train = [ 1 4 11 12 13 14 15 16 17 18 19 20 21 23 24 25];
 
 %pac : people, action, cells
-[list_pac_te total_num_covs_te] = get_list( n_actions, path, all_people, actions, load_sub_path, people_test);
+[list_pac_tr total_num_covs_tr] = get_list( n_actions, path, all_people, actions, load_sub_path, people_train);
 
-vec_TestingSet (path, load_sub_path, list_pac_te, dim );
-vec_Clusters(path, folder_name_cl, K, dim);
-
+vec_TrainingSet (path, load_sub_path, list_pac_tr, dim );
 
 
-for i=1:length(list_pac_te)
+
+
+for i=1:length(list_pac_tr)
     i
-    one_video_pac = {list_pac_te{i,:}};  
+    one_video_pac = {list_pac_tr{i,:}};  
     tic
     [cluster_list_one_video n_points_cl] = assign_points(one_video_pac, K,path, load_sub_path, folder_name_cl);
     toc
