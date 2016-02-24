52567d10fad2c1a5d5886a2afc6b2300b534d5d2
johanna-codes <johacarvajalg@gmail.com>
2016-02-24 11:27:42 +1000

    Changing KORP for KPCA-RP

---
+++ kth/KPCA-RP/KORP_main.m
@@ -0,0 +1,52 @@
+%% Si es mas rapido
+
+clear all
+clc
+dbstop error;
+dbstop in random_projection at 34;
+
+%% INPUTS
+% prompt = 'Number of Iterations ';
+% n_iter = input(prompt);
+
+%prompt = 'Number of Random Points ';
+%R = input(prompt);
+
+
+    
+    
+    
+%%
+path  = '~/codes/codes-git/paper2016a/trunk/kth/';
+dim = 14;
+actions = importdata('actionNames.txt');
+all_people = importdata('people_list.txt');
+scale_factor = 1;
+shift = 0;
+
+people_test =   [ 2 3 5  6  7  8  9  10 22 ];
+people_train = [ 1 4 11 12 13 14 15 16 17 18 19 20 21 23 24 25];
+
+load_sub_path =strcat('overlapped_covariances/Covs/sc1/scale', int2str(scale_factor), '-shift',  int2str(shift));
+
+
+n_people  = length(all_people);
+n_actions = length(actions);
+
+%pac : people, action, #covs
+[list_pac_tr total_num_covs_tr] = get_list( n_actions, path, all_people, actions, load_sub_path, people_train);
+
+%# of random points
+r_points = floor(total_num_covs_tr*10/100);
+
+
+prompt = 'Folder name to save Projected Space ';
+folder_name = strcat(input(prompt, 's'),date,'_R_',num2str(r_points));
+mkdir(pwd, folder_name);
+show_you = strcat('Folder', folder_name);
+disp(show_you);
+
+random_idx_pac = random_points (list_pac_tr, r_points); 
+
+random_projection(random_idx_pac, r_points, path, load_sub_path, folder_name, dim);
+
---
+++ kth/KPCA-RP/get_list.m
@@ -0,0 +1,32 @@
+function [list_pac total_num_covs] = get_list(n_actions, path, all_people, actions, load_sub_path, people_train)
+
+n_people = length(people_train);
+
+
+
+list_pac = cell(n_people*n_actions,3);
+k =1;
+total_num_covs = 0;
+
+
+
+for pe = 1: n_people
+    idx = people_train(pe);
+    %pause
+    
+    for act=1: n_actions
+        
+      list_pac{k,1}  = all_people(idx);
+      list_pac{k,2}  = actions(act);
+      load_Numcov =  strcat( path, load_sub_path, '/NumCov_', all_people(idx), '_', actions(act),  '.dat');
+      num_cov = load( char(load_Numcov) );
+      total_num_covs = total_num_covs + num_cov;
+      list_pac{k,3}  = num_cov;
+      %[list_pac{k,:}]
+      k=k+1;
+      
+        
+    end
+    %pause
+    
+end
