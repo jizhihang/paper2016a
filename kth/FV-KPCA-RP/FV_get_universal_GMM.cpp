66d323df730b01483f1c7713a625490a8080ecbc
johanna-codes <johacarvajalg@gmail.com>
2016-02-25 13:24:43 +1000

    Adding FV

--- kth/FV-KPCA-RP/FV_get_universal_GMM.cpp
+++ kth/FV-KPCA-RP/FV_get_universal_GMM.cpp
@@ -95,10 +95,15 @@ main(int argc, char** argv)
 	//pac : people, action, projected_point (cov_c)
 	rowvec vector_pac;
 	vector_pac.load( load_projected_point_i.str(), hdf5_binary );
-	cout << "Vector Size:" << vector_pac.n_elem << endl;
-	vectors_video_i.col(i-1) = vector_pac.t();
+	//cout << "Vector Size:" << vector_pac.n_elem << endl;
+	if (vector_pac.n_elem >0)
+	{
+	vectors_video_i.col(k) = vector_pac.t();
+	++k;
+	}
       }
       
+      cout << k << end;
       cout << vectors_video_i.n_rows << " - " << vectors_video_i.n_cols << endl;
       
       uni_features	 = join_rows( uni_features, vectors_video_i );
