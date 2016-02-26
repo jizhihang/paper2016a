c2009e596bbc8199e4b8eb246134a21f9dcf04d5
johanna-codes <johacarvajalg@gmail.com>
2016-02-26 16:44:12 +1000

    FV in Matlab with random projection. libLinear

--- kth/FV-KPCA-RP-matlab/FV_svm_train.m
+++ kth/FV-KPCA-RP-matlab/FV_svm_train.m
@@ -33,6 +33,6 @@ data_train = X_train';
 %save(save_svm_model, 'model');
 
 %% libLinear
-model = train(labels_train, X_train, ['-s 0 -t 0 -c 1' ]);
+model = train(labels_train, X_train, ['-s 0 -c 1' ]);
 save_svm_model = strcat( './svm_models_liblinear/linear_kernel_svm_FV_pp_K', num2str(K), '.mat')
 save(save_svm_model, 'model');
