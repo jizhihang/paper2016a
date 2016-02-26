ef74f7c13b6c959198997299144de3f13bf4a9ff
johanna-codes <johacarvajalg@gmail.com>
2016-02-26 12:57:59 +1000

    FV in Matlab with random projection

---
+++ kth/FV-KPCA-RP-matlab/FV_svm_test.m
@@ -0,0 +1,29 @@
+function  [predicted_label, accuracy, dec_values] = FV_svm_test(K, list_pac_te)
+
+load_svm_model = strcat( './svm_models/linear_kernel_svm_FV_pp', num2str(K), '.mat');
+save(load_svm_model, 'model');
+
+
+
+n_samples_test = length(list_pac_te);
+X_test = zeros(K,n_samples_test);
+labels_test = zeros(n_samples_test,1);
+    
+
+for i=1: n_samples_test    
+   
+    
+    person   = list_pac_te{i,1};
+    action   = list_pac_te{i,2};
+    act      = list_pac_te{i,4};
+    
+    load_FV =   strcat('./FV_K', num2str(K), '/FV_', person, '_', action, '.h5' );
+    data_one_FV = hdf5info( char(load_FV) );
+    FV_i = hdf5read(data_one_FV.GroupHierarchy.Datasets(1));
+    
+    X_test(:,i) = FV_i; 
+    labels_test(i) = act;
+    
+end
+
+[predicted_label, accuracy, dec_values] = svmpredict(labels_test, X_test', model);
---
+++ kth/FV-KPCA-RP-matlab/FV_svm_train.m
@@ -0,0 +1,29 @@
+function FV_svm_train(K, list_pac_tr)
+
+n_samples_train = length(list_pac_tr);
+X_train = zeros(K,n_samples_train);
+labels_train = zeros(n_samples_train,1);
+    
+
+for i=1: n_samples_train     
+   
+    
+    person   = list_pac_tr{i,1};
+    action   = list_pac_tr{i,2};
+    act      = list_pac_tr{i,4};
+    
+    load_FV=  strcat('./FV_K', num2str(K), '/FV_', person, '_', action, '.h5' );
+    data_one_hist= hdf5info( char(load_FV) );
+    FV_i = hdf5read(data_one_hist.GroupHierarchy.Datasets(1));
+    
+    
+    
+    X_train(:,i) = FV_i; 
+    labels_train(i) = act;
+    
+end
+
+data_train = X_train';
+model = svmtrain(labels_train, data_train, ['-s 0 -t 0 ' ]);
+save_svm_model = strcat( './svm_models/linear_kernel_svm_FV_pp', num2str(K), '.mat');
+save(save_svm_model, 'model');
