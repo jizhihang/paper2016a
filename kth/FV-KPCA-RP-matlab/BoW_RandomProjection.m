400bdd2bce8e7ab266014816baaeee28e93184ce
johanna-codes <johacarvajalg@gmail.com>
2016-02-29 10:07:03 +1000

    BoW RP

--- kth/FV-KPCA-RP-matlab/BoW_RandomProjection.m
+++ kth/FV-KPCA-RP-matlab/BoW_RandomProjection.m
@@ -36,7 +36,7 @@ n_actions = length(actions);
 [list_pac_te total_num_covs_te] = get_list( n_actions, path, all_people, actions, load_sub_path_1, people_test);
 
 %vec_K = [128 256 512 4000];
-vec_K = [512 ];
+vec_K = [512 4000 ];
 
 for k =1:length(vec_K)
     K = vec_K(k)
