c3e0e769dd8626eabc825d01b32a7ad0a350715a
johanna-codes <johacarvajalg@gmail.com>
2016-03-01 10:07:07 +1000

    BoW RP

--- kth/FV-KPCA-RP-matlab/BoW_RandomProjection.m
+++ kth/FV-KPCA-RP-matlab/BoW_RandomProjection.m
@@ -78,7 +78,7 @@ all_accuracy = zeros(1,length(vec_K));
      K = vec_K(k)
      BoW_svm_train(K, list_pac_tr, dim);
      [predicted_label, accuracy, dec_values] = BoW_svm_test(K, list_pac_te, dim);
-     all_accuracy(k) = accuracy(i);
+     all_accuracy(k) = accuracy(1);
 
 end
 
