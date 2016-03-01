5ca9ce6c5349ac1811f7d3ba2cf678e4b36d4c87
johanna-codes <johacarvajalg@gmail.com>
2016-03-01 12:54:39 +1000

    Improving Code for FV  RP

--- kth/FV-KPCA-RP-matlab/FisherVector_RandomProjection.m
+++ kth/FV-KPCA-RP-matlab/FisherVector_RandomProjection.m
@@ -12,7 +12,6 @@ addpath('/home/johanna/toolbox/yael/matlab');
 
 %General paths
 path  = '~/codes/codes-git/paper2016a/trunk/kth/';
-load_sub_path_1 =strcat('overlapped_covariances/Covs/sc1/scale', int2str(scale_factor), '-shift',  int2str(shift));
 
 
 %svm_type
@@ -49,6 +48,8 @@ actions = importdata('actionNames.txt');
 all_people = importdata('people_list.txt');
 scale_factor = 1;
 shift = 0;
+load_sub_path_1 =strcat('overlapped_covariances/Covs/sc1/scale', int2str(scale_factor), '-shift',  int2str(shift));
+
 
 people_test =  [ 2 3 5  6  7  8  9  10 22 ];
 people_train = [ 1 4 11 12 13 14 15 16 17 18 19 20 21 23 24 25];
