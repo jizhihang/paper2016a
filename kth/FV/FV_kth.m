f0def3cc32aa6869eec484579bce06bd695b8c03
johanna-codes <johacarvajalg@gmail.com>
2016-03-04 15:20:41 +1000

    FV in the normal euclidean space for comparisson purposes

--- kth/FV/FV_kth.m
+++ kth/FV/FV_kth.m
@@ -24,7 +24,9 @@ path_features = '/home/johanna/codes/codes-git/manifolds/trunk/kth/dim_14/featur
 
 
 dim = 14;
-K = 256;
+vec_K =  [1024 512 256 128];
+
+%K = 256;
 n_iterGMM = 10; % For GMM
 
 actions = importdata('actionNames.txt');
@@ -34,30 +36,43 @@ all_people = importdata('people_list.txt');
 n_peo =  size(all_people,1);
 
 %n_test = (n_peo-1)*n_actions;
-dim_FV = 2*dim*K;
+
 
 people_test =  [ 2 3 5  6  7  8  9  10 22 ];
 people_train = [ 1 4 11 12 13 14 15 16 17 18 19 20 21 23 24 25];
 
 GMM_folder = 'universal_GMM';
 svm_folder = 'svm_models';
-FV_folder = strcat('FV_K', num2str(K));
 
-create_folders_FV(FV_folder, svm_folder, GMM_folder);
+for k =1:length(vec_K)
+    
+    K = vec_K(k)
+    
+    FV_folder = strcat('FV_K', num2str(K));
+    dim_FV = 2*dim*K;
+    create_folders_FV(FV_folder, svm_folder, GMM_folder);
+    
+    get_universalGMM(path_features, people_train, all_people, actions,  K, dim, n_iterGMM, GMM_folder)
+    FV_kth_all_videos(path_features, all_people, actions, K, dim, GMM_folder, FV_folder)
+    
+end
 
-%get_universalGMM(path_features, people_train, all_people, actions,  K, dim, n_iterGMM, GMM_folder)
-%FV_kth_all_videos(path_features, all_people, actions, K, dim, GMM_folder, FV_folder)
 
 vec_c = [ 0.1 1 10 100 1000 10000];
-all_accuracy = zeros(1, length(vec_c) );
-for j = 1: length(vec_c)
-    c = vec_c (j);
+all_accuracy = zeros(length(vec_K), length(vec_c) );
+for k =1:length(vec_K)
     
-    params =  sprintf('-s 0 -t 0 -c %f -q', c);
-    FV_train(people_train, all_people, actions,  K, dim, dim_FV, FV_folder, svm_folder, params);
-    [predicted_label, accuracy, dec_values]  = FV_test(people_test, all_people, actions,  K, dim, dim_FV, FV_folder, svm_folder);
+    K = vec_K(k)
+    for j = 1: length(vec_c)
+        c = vec_c (j);
+        
+        params =  sprintf('-s 0 -t 0 -c %f -q', c);
+        FV_train(people_train, all_people, actions,  K, dim, dim_FV, FV_folder, svm_folder, params);
+        [predicted_label, accuracy, dec_values]  = FV_test(people_test, all_people, actions,  K, dim, dim_FV, FV_folder, svm_folder);
+        
+        all_accuracy(k, j) = accuracy(1);
+    end
     
-    all_accuracy(j) = accuracy(1);
 end
 
 
