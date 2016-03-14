fc05b7c04d417fe1fbb4c4be6ee82c3f37643db2
johanna-codes <johacarvajalg@gmail.com>
2016-03-14 15:11:27 +1000

    Adding PCA for KPCA

---
+++ kth/KPCA-RP/KPCA_RP_main_PCA.m
@@ -0,0 +1,55 @@
+% After using random projection points. Do PCA over new points 
+
+
+clear all
+clc
+dbstop error;
+%dbstop in random_projection at 38;
+%dbstop in project_points at 34
+
+%% INPUTS
+% prompt = 'Number of Iterations ';
+% n_iter = input(prompt);
+
+%prompt = 'Number of Random Points ';
+%R = input(prompt);
+
+
+    
+ %Kernel Type:   
+ kernel_type = 'poly'
+ %kernel_type = 'stein'
+   
+%%
+path  = '~/codes/codes-git/paper2016a/trunk/kth/';
+dim = 14;
+actions = importdata('actionNames.txt');
+all_people = importdata('people_list.txt');
+scale_factor = 1;
+shift = 0;
+
+people_test =  [ 2 3 5  6  7  8  9  10 22 ];
+people_train = [ 1 4 11 12 13 14 15 16 17 18 19 20 21 23 24 25];
+
+load_sub_path =strcat('overlapped_covariances/Covs/sc1/scale', int2str(scale_factor), '-shift',  int2str(shift));
+
+
+n_people  = length(all_people);
+n_actions = length(actions);
+
+%pac : people, action, #covs
+[list_pac_tr total_num_covs_tr] = get_list( n_actions, path, all_people, actions, load_sub_path, people_train);
+[list_pac_te total_num_covs_te] = get_list( n_actions, path, all_people, actions, load_sub_path, people_test);
+
+
+r_points = floor(8475/10);
+folder_name = strcat('pca_projected_points_dim', num2str(r_points))
+
+
+% Get projected points for Training and Testing Set
+disp('PCA for projected points for Training Set');
+PCA_over_projected_points (list_pac_tr,path, load_sub_path, r_points, folder_name);
+
+disp('PCA for projected points for for Testing Set');
+PCA_over_projected_points (list_pac_te, path, load_sub_path,r_points, folder_name);
+
---
+++ kth/KPCA-RP/PCA_over_projected_points.m
@@ -0,0 +1,31 @@
+function PCA_over_projected_points (list_pac, folder_name)
+
+
+    
+    for i=1: length(list_pac)
+        
+        person =  list_pac{i,1};
+        action =  list_pac{i,2};
+        num_covs = list_pac{i,3};
+        %show_you = strcat(person,  '_', action);
+        %disp(show_you);
+        
+        %parfor c  = 1:num_covs
+        for c = 1:num_covs
+           
+            
+            load_pp_vector =  strcat('./', folder_name, '/pp_', person, '_', action,  '_segm', num2str(c) , '.h5' );
+            S = char(load_pp_vector);
+            data_one_cov= hdf5info(S);
+            %one_ppoint = hdf5read(data_one_cov.GroupHierarchy.Datasets(1)); % One covariance point
+            
+            
+            %pca_one_point = 0;
+            %save_pp =  strcat('./', folder_name, '/pp_', person, '_', action,  '_segm', num2str(c) , '.h5' );
+            %hdf5write(char(save_pp), '/dataset1', pca_one_point);
+            
+        end
+    end
+    
+
+
