8fd925891cd69b66adc8d169fe1c65cab2225cd3
johanna-codes <johacarvajalg@gmail.com>
2016-03-14 17:14:04 +1000

    Adding PCA for FV-KPCA-RP-matlab

--- kth/FV-KPCA-RP-matlab/get_universalGMM.m
+++ kth/FV-KPCA-RP-matlab/get_universalGMM.m
@@ -15,7 +15,7 @@ for i=1: length(list_pac)
     %parfor c  = 1:num_covs 
     for c = 1:num_covs 
         %Loading Projected Point
-        load_pp_vector =  strcat( path, folder_pp, '/pp_', person, '_', action,  '_segm', num2str(c) , '.h5' );
+        load_pp_vector =  strcat( path, folder_pp, '/pp_', person, '_', action,  '_segm', num2str(c) , '.h5' )
         S = char(load_pp_vector);
         data_one_cov= hdf5info(S);
         xi = hdf5read(data_one_cov.GroupHierarchy.Datasets(1)); % One covariance point
