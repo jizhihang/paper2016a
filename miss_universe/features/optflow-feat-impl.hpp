dc6b15af1bb0eee6ce73d02ef73b262397da2ca2
johanna-codes <johacarvajalg@gmail.com>
2016-03-15 15:38:33 +1000

    Adding Features for Miss Universe

--- miss_universe/features/optflow-feat-impl.hpp
+++ miss_universe/features/optflow-feat-impl.hpp
@@ -210,9 +210,9 @@ opt_feat::feature_video( std::string one_folder, Struct_feat_lab &my_Struct_feat
     cout << ss_frame_name.str() << endl;
     
     
-    
-    frame_tmp = cv::imread( ss_frame_name.str());
     /*
+    frame_tmp = cv::imread( ss_frame_name.str());
+    
     BB = bb_list.row(fr);
     //BB.print();
 
