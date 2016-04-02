683759096d1f4647b2801248b7005b1d363abb9f
johanna-codes <johacarvajalg@gmail.com>
2016-04-02 20:23:53 +1000

    dataset_info

---
+++ miss_universe/SFV/dataset_info.m
@@ -0,0 +1,36 @@
+
+
+
+%Path for Original dataset
+path_dataset  = '/home/johanna/codes/datasets_codes/MissUniverse/';
+all_years = [  2010 2007 2003 2002 2001 2000 1999 1998 1997 1996];
+
+dataset_description = cell{105,3};
+
+
+for y=1:length(all_years)
+    
+    year = num2str( all_years(y) );
+    load_year_list =  strcat(path_dataset, 'MissUniverse', year, '/country_list.txt');
+    
+    countries = importdata(load_year_list);
+    n_countries = length(countries);
+    
+    
+    for c=1:n_countries
+         load_year_list =  strcat(path_dataset, 'MissUniverse', year, countries(c), '-', num2str(1), '/list.txt');
+         frame_list = importdata(load_year_list);
+         
+        length(frame_list)
+        
+        
+    end
+    
+    
+    
+    
+    
+end
+    
+
+
