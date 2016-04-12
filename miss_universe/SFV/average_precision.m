b3efa8bfc03cb77664e07e845fc62207cfff37c6
johanna-codes <johacarvajalg@gmail.com>
2016-04-12 22:23:42 +1000

    Adding average_precision

---
+++ miss_universe/SFV/average_precision.m
@@ -0,0 +1,31 @@
+function AP = average_precision(predicted_output, labels_test)
+
+%Precision at position n (P@n)
+%Mean average precision (MAP)
+%LETOR: Benchmark Dataset for Research on Learning to Rank for Information Retrieval
+
+%predicted_output 
+%labels_test
+
+list_Pn = [];
+tmp_sum = 0;
+for b=1:length(predicted_output)
+    
+  tmp = (predicted_output(b) == labels_test(b));
+  tmp_sum = tmp_sum + tmp;
+  list_Pn = [list_Pn  tmp_sum/b*tmp ];
+    
+end
+AP = list_Pn;
+
+%Example from 
+%LETOR: Benchmark Dataset for Research on Learning to Rank for Information Retrieval
+
+%list = [1 0 0 1 1 1 0 0 1 1 ];
+%list_Pn = [];
+
+%for j=1:10
+%    list_Pn = [ list_Pn sum(list(1:j))/j  ];
+%end
+
+%AP = (sum(list_Pn.*list))/sum(list);
