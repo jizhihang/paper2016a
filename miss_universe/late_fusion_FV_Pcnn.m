576b72a7dc8fc32177c9ba0fe0bd8496693a3311
johanna-codes <johacarvajalg@gmail.com>
2016-05-19 20:50:34 +1000

    Late Fusion FV Pcnn

--- miss_universe/late_fusion_FV_Pcnn.m
+++ miss_universe/late_fusion_FV_Pcnn.m
@@ -7,6 +7,10 @@ load('./P-CNN/info_results_Pcnn', 'info_results', 'all_years');
 %info_results(i,1) -> accuracy(1)
 %info_results(i,2) -> real_order
 %info_results(i,3) -> predicted_order
+%info_results(i,4) -> labels_test; (Real binary labels)
+%info_results(i,5) -> predicted_output;
+
+
 real_labels = info_results(i,2)
 
 
