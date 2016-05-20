74f49882bcd23cdcd04ada11f0198ba9537c8d96
johanna-codes <johacarvajalg@gmail.com>
2016-05-20 10:59:51 +1000

    Adding Late Fusion SFV_PCNN

--- miss_universe/Fusion/late_fusion_FV_Pcnn.m
+++ miss_universe/Fusion/late_fusion_FV_Pcnn.m
@@ -4,7 +4,7 @@ my_path = pwd;
 project_path = fileparts(my_path);
 
 load( [ project_path '/FV/dec_values_FV.mat']);
-load( [project_path '/P-CNN/dec_values_Pcnn.mat']);
+load( [ project_path '/P-CNN/dec_values_Pcnn.mat']);
 
 load([ project_path '/P-CNN/info_results_Pcnn'], 'info_results', 'all_years');
 load([ project_path '/FV/info_results_FV'], 'info_results_FV' );
