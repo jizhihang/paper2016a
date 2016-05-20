36e5a0a9c38fb14a355a5dac67f38b492daf47e4
johanna-codes <johacarvajalg@gmail.com>
2016-05-20 11:55:44 +1000

    Adding scripts to concatenate FV and SFV

---
+++ miss_universe/Fusion/concatenate_FV_SFV.m
@@ -0,0 +1,168 @@
+% UQ && Server (WANDA) && Home
+clear all
+close all
+clc
+
+dbstop error;
+%dbstop in  get_universalGMM at 14
+%dbstop in FV_layers_1 at 89
+%dbstop in dim_reduction at 48
+
+%%Setting paths for libs and features and original dataset
+pc = 'wanda'; % uq wanda home
+svm_type = 'linear'; %'svm';    %libsvm
+[path_dataset path_features] = set_paths(pc, svm_type);
+all_years = [  2010 2007 2003 2002 2001 2000 1999 1998 1997 1996];
+
+
+%Parameters
+%vec_c = [ 0.1 1 10 100];
+vec_c = [ 10 ] ;
+K = 256;
+view = 1;
+
+
+
+% For SFV
+FV_folder_ly1 = strcat('layer1/FV_K', num2str(K));
+FV_folder_ly2 = strcat('layer2/FV_K', num2str(K));
+
+
+
+% crear params using libSVM
+
+if  strcmp( svm_type, 'linear')
+    s = 1; % L2-regularized L2-loss support vector classification (dual)
+    all_accuracy = zeros(length(all_years), length(vec_c) );
+    %all_AP = zeros(length(all_years), length(vec_c) );
+end
+
+
+
+info_results = cell(length( all_years), 5);
+all_predicted_order = cell(length( all_years),1);
+all_real_order = cell(length( all_years),1);
+
+
+my_path = pwd;
+project_path = fileparts(my_path);
+
+param.path_datasets = path_dataset;
+param.project_path = project_path;
+param.view = view;
+param.K = K;
+
+for i = 1: length( all_years)
+    
+    run = i;
+    years_train  =  all_years;
+    years_train(i) = [];
+    years_test  = all_years(i) ;
+    
+   
+
+    
+    
+    % Dimensionality of SFV
+    load_rp_data = strcat(project_path, '/', FV_folder_ly1, '/pca_projection_data_run', num2str(run));
+    load(char(load_rp_data),'NP');
+    
+    dim = NP;
+    dim_FV = 2*dim*K;
+    
+    param.years_train = years_train;
+    param.dim_FV = dim_FV;
+    param.FV_folder_ly2 = FV_folder_ly2;
+    param.run = run;
+
+
+    
+    
+    for j = 1: length(vec_c)
+        c = vec_c (j);
+        
+        if strcmp( svm_type, 'linear')
+            params_svm =  sprintf('-s %f  -c %f -q', s, c);
+        end
+        
+        
+        
+        %Training
+        train_rankSVM_both(params, params_svm, svm_folder, svm_type);
+        
+        %Testing
+        %[predicted_output, accuracy, dec_values, labels_test,  n_labels_test, scores, n_countries]  = FV_test_rankSVM(path_dataset, view, years_test, K, dim_FV, FV_folder_ly2, svm_folder, svm_type, run);
+        %AA = [predicted_output  labels_test];
+        %all_accuracy(i,j) = accuracy(1);
+        
+    
+        
+        
+        %[a real_order]  = sort(scores', 'descend');
+        %BB  = [ predicted_output n_labels_test];
+        %predicted_order = get_predicted_list(BB, n_countries);
+       
+        
+    end
+    
+% Solo lo puedo hacer cuando vec_c tiene un solo elemento:
+%     all_predicted_order(i) = {predicted_order};
+%     all_real_order(i) = {real_order};
+%     
+%     info_results_SFV(i,1) = {accuracy(1)};
+%     info_results_SFV(i,2) = {real_order};
+%     info_results_SFV(i,3) = {predicted_order};
+%     info_results_SFV(i,4) = {labels_test};
+%     info_results_SFV(i,5) = {predicted_output };
+%     
+    
+end
+
+
+
+
+%To visualise
+%all_predicted_order{i}
+
+% disp('Top @ p = length(real)')
+% all_ndcg = [];
+% for m=1:length( all_years)
+% info_results{m,1}; 
+% real= info_results{m,2}; 
+% pred=info_results{m,3};
+% p = length(real);
+% real_scores = p:-1:1; %pred_scores = zeros(1,p);
+% 
+% k = p;
+% for v=1:p
+%     pred_scores(v) = real_scores(find(real==pred(v))); 
+% end
+% 
+% c_ndcg = [ ndcg(pred_scores,real_scores,k, 1) ndcg(pred_scores,real_scores,k, 2) ndcg(pred_scores,real_scores,k, 3)];
+% all_ndcg = [all_ndcg;c_ndcg];
+% end
+% all_ndcg = all_ndcg*100
+% mean(all_ndcg)
+
+
+% % Top:
+% 
+% top_k = 1; %1 or 3 or 5
+% acc = 0;
+% for m =1:length( all_years)
+%     info_results{m,1};real= info_results{m,2};pred=info_results{m,3};
+%     winner = real(1);
+%     top = pred(1:top_k);
+%     acc = acc + length(find(top ==winner));
+%     %disp('**************')
+%     %pause
+% end
+% 
+% acc = acc/ length( all_years)
+   
+
+
+
+
+
+
---
+++ miss_universe/Fusion/train_rankSVM_both.m
@@ -0,0 +1,93 @@
+function train_rankSVM_both(params, params_svm, svm_folder, svm_type)
+
+
+path_dataset = params.path_dataset;
+project_path = params.project_path;
+view = params.view;
+years_train = params.years_train;
+K = params.K;
+dim_FV = params.dim_FV;
+FV_folder_ly2 = params.FV_folder_ly2;
+run = params.run;
+
+
+
+
+n_years = length(years_train);
+
+n_samples_train = 0;
+n_comparisons = 0;
+for y=1:n_years
+    year = num2str( years_train(y) );
+    load_year_list =  strcat(path_dataset, 'MissUniverse', year, '/country_list.txt');
+    countries = importdata(load_year_list);
+    n_countries = length(countries);
+    n_samples_train = n_samples_train + n_countries;
+    %n_comparisons = n_comparisons + n_countries*(n_countries-1);%case A 
+    n_comparisons = n_comparisons + nchoosek(n_countries,2);%case B
+   
+end
+
+
+X_train = zeros( dim_FV,  n_comparisons );
+labels_train = zeros(n_comparisons,1);
+n_labels_train = zeros(n_comparisons,2);
+j = 1;
+
+
+for y=1:n_years
+    
+    year = num2str( years_train(y) );
+    load_year_list =  strcat(path_dataset, 'MissUniverse', year, '/country_list.txt');
+    
+    countries = importdata(load_year_list);
+    n_countries = length(countries);
+    
+    load_scores_list  =  strcat(path_dataset, 'MissUniverse', year, '/scores.txt');
+    fileID = fopen(load_scores_list);
+    countries_scores  = textscan(fileID,'%s %f');
+    countries_2       = countries_scores{1};
+    scores            = countries_scores{2};
+    
+    
+    
+    for c = 1:n_countries
+        
+        load_FV =  strcat(project_path, '/SFV/', FV_folder_ly2, '/MissUniverse', year, '/', countries(c), '_view', num2str(view), '_run', num2str(run),'.h5' );
+        S = char(load_FV);
+        FV_one_video= hdf5info(S);
+        FV1 = hdf5read(FV_one_video.GroupHierarchy.Datasets(1)); 
+        
+        %for c2 = 1 : n_countries %case A
+        for c2 = c + 1 : n_countries %case B
+         
+                load_FV =  strcat(project_path, '/SFV/', FV_folder_ly2, '/MissUniverse', year, '/', countries(c2),  '_view', num2str(view) , '_run', num2str(run) ,'.h5' );
+                S = char(load_FV);
+                FV_one_video= hdf5info(S);
+                FV2 = hdf5read(FV_one_video.GroupHierarchy.Datasets(1)); 
+                
+                X_train(:,j) = FV1-FV2;
+                
+                n_labels_train (j,1)  = c;
+                n_labels_train (j,2)  = c2;
+                
+                if (scores(c)>scores(c2))
+                    
+                    labels_train(j) = 1;
+                else
+                    labels_train(j) = -1;
+                end
+                
+                j = j + 1;
+        end
+    end
+end
+      
+
+if strcmp( svm_type, 'linear')
+    %svm_type
+    sparse_X_train =  sparse(X_train');
+    model = train(labels_train, sparse_X_train, [params_svm]);
+    save_svm_model = strcat( './', svm_folder, '/concFV_SFV_K', num2str(K), '_view', num2str(view), '_run', num2str(run), '.mat');
+    save(save_svm_model, 'model');
+end
